fe73997adbe43859ab6079340a0df085262c31b4
jacobnunno <34197041+jacobnunno@users.noreply.github.com>
2020-12-14 13:48:43 -0500

    Added error handling to the checklink function and removed the console logs

--- commands/checklink.js
+++ commands/checklink.js
@@ -24,25 +24,32 @@ async function scrapeNortonLinkChecker(message, url)
     const browser = await puppeteer.launch({
         args: ["--no-sandbox"]
     });
-    const page = await browser.newPage();
-    await page.goto(nortonURL);
 
-    //grab the correct element
-    const [nortonRating] = await page.$x('//*[@id="bodyContent"]/div/div/div[3]/div[1]/div[1]/div[2]/div[1]/div/b');
-    const txt = await nortonRating.getProperty('textContent');
-    const nortonRatingTxt = await txt.jsonValue();
+    try{
+        const page = await browser.newPage();
+        await page.goto(nortonURL);
 
-    const [nortonCommunityRating] = await page.$x('//*[@id="comRating"]/div/div[2]/label');
-    const txt2 = await nortonCommunityRating.getProperty('textContent');
-    const nortonCommunityRatingTxt = await txt2.jsonValue();
+        //grab the correct element
+        const [nortonRating] = await page.$x('//*[@id="bodyContent"]/div/div/div[3]/div[1]/div[1]/div[2]/div[1]/div/b');
+        const txt = await nortonRating.getProperty('textContent');
+        const nortonRatingTxt = await txt.jsonValue();
 
-    browser.close();
-    message.channel.send("Norton Report:\n" + "\tNorton Rating: " + nortonRatingTxt + "\n" + "\tNorton Community Rating: " + nortonCommunityRatingTxt);
-    //delete the message if Norton finds that the message contains an unsafe link
-    if (nortonRatingTxt == "WARNING") {
-        message.delete()
-        .then(message.channel.send('Deleted message from because it contained an unsafe link'));
-     }
+        const [nortonCommunityRating] = await page.$x('//*[@id="comRating"]/div/div[2]/label');
+        const txt2 = await nortonCommunityRating.getProperty('textContent');
+        const nortonCommunityRatingTxt = await txt2.jsonValue();
+
+        message.channel.send("Norton Report:\n" + "\tNorton Rating: " + nortonRatingTxt + "\n" + "\tNorton Community Rating: " + nortonCommunityRatingTxt);
+        //delete the message if Norton finds that the message contains an unsafe link
+        if (nortonRatingTxt == "WARNING") {
+            message.delete()
+            .then(message.channel.send('Deleted message from because it contained an unsafe link'));
+         }
+    } catch(err){
+        console.log(err);
+        message.channel.send('There was an error getting Norton report. Please try again');
+    } finally{
+        await browser.close();
+    }
 }
 
 async function scrapeTransparencyReportLinkChecker(message, url)
@@ -57,19 +64,29 @@ async function scrapeTransparencyReportLinkChecker(message, url)
     const browser = await puppeteer.launch({
         args: ["--no-sandbox"]
     });
-    const page = await browser.newPage();
-    await page.goto(TrURL);
+    try{
+        const page = await browser.newPage();
+        await page.goto(TrURL);
 
-    //grab the correct element
-    const [TransparencyReportRating] = await page.$x('//*[@id="scrolling-element"]/safe-browsing-report/ng-component/site-status-result/report-section/section/div/data-tile/div[2]/span');
-    const txt = await TransparencyReportRating.getProperty('textContent');
-    const TransparencyReportRatingTxt = await txt.jsonValue();
+        //grab the correct element
+        const [TransparencyReportRating] = await page.$x('//*[@id="scrolling-element"]/safe-browsing-report/ng-component/site-status-result/report-section/section/div/data-tile/div[2]/span');
+        const txt = await TransparencyReportRating.getProperty('textContent');
+        const TransparencyReportRatingTxt = await txt.jsonValue();
 
-    browser.close();
-    
-    message.channel.send("Google Transparency Report:\n" + "\t" + TransparencyReportRatingTxt);
-    if (TransparencyReportRatingTxt == "Some pages on this site are unsafe") {
-        message.delete()
-        .then(message.channel.send('Deleted message from because it contained an unsafe link'));
-     }
+        if (TransparencyReportRatingTxt === "Check a specific URL") {
+            message.channel.send("Google Transparency Report:\n\t It's hard to provide a simple safety status for sites like www.google.com, which have a lot of content. Sites that are" +
+            " generally safe sometimes contain some unsafe content (for example, in blogs or comments). For more detailed safety info, check a specific directory or webpage.");
+        }else{
+            message.channel.send("Google Transparency Report:\n" + "\t" + TransparencyReportRatingTxt);
+        }
+        if (TransparencyReportRatingTxt == "Some pages on this site are unsafe") {
+            message.delete()
+            .then(message.channel.send('Deleted message from because it contained an unsafe link'));
+        }
+    } catch(err){
+        console.log(err);
+        message.channel.send('There was an error getting Google Transparency Report. Please try again');
+    } finally{
+        await browser.close();
+    }
 }
