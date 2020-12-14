78b84341f25df3c243fd7329d8a269f674dd553c
jacobnunno <34197041+jacobnunno@users.noreply.github.com>
2020-12-14 16:38:20 -0500

    vcxz

--- commands/checklink.js
+++ commands/checklink.js
@@ -8,8 +8,7 @@ module.exports = {
     name:'checklink',
     description: "Command to check a link provided by a user",
     execute(message, link){
-        //message.channel.send('Link found');
-        //message.channel.send(link);
+        //call the functions to check the links
         scrapeNortonLinkChecker(message, link);
         scrapeTransparencyReportLinkChecker(message, link)
     }
@@ -24,7 +23,8 @@ async function scrapeNortonLinkChecker(message, url)
     const browser = await puppeteer.launch({
         args: ["--no-sandbox"]
     });
-
+    
+    //try and catch for if the website doesn't load properly
     try{
         const page = await browser.newPage();
         await page.goto(nortonURL);
@@ -69,7 +69,7 @@ async function scrapeTransparencyReportLinkChecker(message, url)
         await page.goto(TrURL);
 
         //grab the correct element
-        const [TransparencyReportRating] = await page.$x('//*[@id="scrolling-element"]/safe-browsing-report/ng-component/site-status-result/report-section/section/div/data-tile/div[2]/span');
+        const [TransparencyReportRating] = await page.$x('/html/body/app/site-layout/mat-sidenav-container/mat-sidenav-content/safe-browsing-report/ng-component/site-status-result/report-section/section/div/data-tile/div[2]/span');
         const txt = await TransparencyReportRating.getProperty('textContent');
         const TransparencyReportRatingTxt = await txt.jsonValue();
 
